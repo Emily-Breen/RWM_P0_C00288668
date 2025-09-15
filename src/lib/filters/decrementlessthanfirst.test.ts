404f65081b4501f48891fd163b3875ab5592ec34
Emily Breen <c00288668@setu.ie>
2025-09-15 12:24:44 +0100

    added in fixed test case spotted by Stephen all tests pass

--- src/lib/filters/decrementlessthanfirst.test.ts
+++ src/lib/filters/decrementlessthanfirst.test.ts
@@ -1,6 +1,6 @@
 import {describe, it, expect} from 'vitest';
 import { decrementlessthanfirst } from './decrementlessthanfirst';
-
+/*pass test case */
 describe('decrementIfLessThanFirst', () => {
   it('works with sample input', () => {
     const input = [4, 2, 8, 3, 9, 4, 10, 5, 11, 6, 1];
@@ -8,11 +8,11 @@ describe('decrementIfLessThanFirst', () => {
     expect(decrementlessthanfirst(input)).toEqual(output);
   });
 });
-
+/*fail test case */
 describe('decrementIfLessThanFirst', () => {
 it ('fail with wrong output', () => {
     const input = [5,2,6,3];
-    const incorrectOutput = [5,2,6,3]
+    const incorrectOutput = [5,1,6,2]
     expect(decrementlessthanfirst(input)).toEqual(incorrectOutput);
 });
 });
